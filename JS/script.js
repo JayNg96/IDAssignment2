c0676882c07dd395db51cf0408a7362f32563a17
JayNg96 <S10208269@connect.np.edu.sg>
2020-12-24 18:35:32 +0800

    Created a JS script to search for digimon

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,31 @@
 const digimonList = document.getElementById('digimonList');
+const searchBar = document.getElementById('searchBar');
 
+// addeventlistener to exclude character whose name doesn't match the word typed in the search box from the api.
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = digimonCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.level.toLowerCase().includes(searchString)
+        );
+    });
+    displayDigimon(filteredCharacters);
+});
+
+//fetch api
 const getDigimon = async () => {
     try { 
         const res = await fetch('https://digimon-api.herokuapp.com/api/digimon'); //This await keyword causes the JavaScript runtime to pause my code, not allowing further code to execute in the meantime until the async function call has returned its result
         digimonCharacters = await res.json();
-        displayDigimon(digimonCharacters);
+        displayDigimon(digimonCharacters); 
     } catch (err) { //Catch error
         console.error(err);
     }
 };
 
+//declare a const to get value from API and return it
 const displayDigimon = (characters) => {
     const htmlString = characters.map((character) => 
     {
@@ -25,4 +41,5 @@ const displayDigimon = (characters) => {
     digimonList.innerHTML = htmlString;
 };
 
+//execute the const getDigimon
 getDigimon();
