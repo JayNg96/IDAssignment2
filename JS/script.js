b1a779bfe3844bd87d550db1349f72ee59256cc3
JayNg96 <S10208269@connect.np.edu.sg>
2020-12-24 17:06:47 +0800

    Display the data that are retrieved from API

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,25 @@ const getDigimon = async () => {
     try { 
         const res = await fetch('https://digimon-api.herokuapp.com/api/digimon'); //This await keyword causes the JavaScript runtime to pause my code, not allowing further code to execute in the meantime until the async function call has returned its result
         digimonCharacters = await res.json();
-        console.log(digimonCharacters); //Console log to see what data i will get
+        displayDigimon(digimonCharacters);
     } catch (err) { //Catch error
         console.error(err);
     }
 };
+
+const displayDigimon = (characters) => {
+    const htmlString = characters.map((character) => 
+    {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>Level: ${character.level}</p>
+                <img src="${character.img}"></img>
+            </li>
+        `;
+    })
+        .join('');
+    digimonList.innerHTML = htmlString;
+};
+
+getDigimon();
