998c5efb456a2921e4a1570bee50fde4766e4532
JayNg96 <S10208269@connect.np.edu.sg>
2020-12-27 18:42:00 +0800

    Validated and formatted html,css,js files

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
 const digimonList = document.getElementById('digimonList');
 const searchBar = document.getElementById('searchBar');
+let digimonCharacters = [];
 
 // addeventlistener to exclude character whose name doesn't match the word typed in the search box from the api.
-try
-{
+try {
     searchBar.addEventListener('keyup', (e) => {
         const searchString = e.target.value.toLowerCase();
-    
+
         const filteredCharacters = digimonCharacters.filter((character) => {
             return (
                 character.name.toLowerCase().includes(searchString) ||
@@ -15,26 +15,21 @@ try
         });
         displayDigimon(filteredCharacters);
     });
-} 
-catch{}
+} catch {}
 
 
 //fetch api
 const getDigimon = async () => {
-    try 
-    { 
+    try {
         const res = await fetch('https://digimon-api.herokuapp.com/api/digimon'); //This await keyword causes the JavaScript runtime to pause my code, not allowing further code to execute in the meantime until the async function call has returned its result
         digimonCharacters = await res.json();
-        displayDigimon(digimonCharacters); 
-    } 
-    catch
-    {}
+        displayDigimon(digimonCharacters);
+    } catch {}
 };
 
 //declare a const to get value from API and return it
 const displayDigimon = (characters) => {
-    const htmlString = characters.map((character) => 
-    {
+    const htmlString = characters.map((character) => {
             return `
             <li class="character">
                 <h2>${character.name}</h2>
@@ -42,7 +37,7 @@ const displayDigimon = (characters) => {
                 <img src="${character.img}"></img>
             </li>
         `;
-    })
+        })
         .join('');
     digimonList.innerHTML = htmlString;
 };
@@ -56,11 +51,9 @@ const close_btn = document.querySelector('.close-btn');
 const social_panel_container = document.querySelector('.social-panel-container');
 
 floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
+    social_panel_container.classList.toggle('visible')
 });
 
 close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
+    social_panel_container.classList.remove('visible');
 });
-
-// Bootstrap Form JS
